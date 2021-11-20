649b3e75dbeefdfe2d0fed052622eb0a7a774f92
fither <onur.hamarattt@gmail.com>
2021-11-20 18:32:38 +0300

    latest: still not work properly

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,115 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div
+      contenteditable="true"
+      id="fake-textarea"
+      class="fake-textarea"
+      @input="onInput"
+      v-html="result"
+      spellcheck="false"
+    ></div>
+    <p>
+      phrase: {{ this.phrase }}
+    </p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      keyword: '',
+      result: '',
+      grayListed: [],
+      blackListed: [],
+      undefined: [],
+      phrase: '',
+      secondsBeforeCheck: .5,
+      timeout: null
+    }
+  },
+  components: {},
+  methods: {
+    async check() {
+      if(!this.keyword.length) {
+        return;
+      }
+      const url = 'https://api.sikayetvar.com/dictionary/phrase/check';
+      const { data } = await axios.post(
+        url, 
+        this.keyword,
+        {
+          headers: {
+            'Content-Type': 'text/plain'
+          }
+        }
+      );
+      
+      this.grayListed = data.grayListed;
+      this.blackListed = data.blackListed;
+      this.undefined = data.undefined;
+      this.phrase = data.phrase;
+
+      this.result = '';
+      this.keyword.split(' ').map((newWord) => {
+        const word = newWord.replace(' ', '').toLowerCase();
+        const grayIndex = this.grayListed.indexOf(word);
+        const blackIndex = this.blackListed.indexOf(word);
+        const undefinedIndex = this.undefined.indexOf(word);
+        if(grayIndex !== -1) {
+          this.result += `<span class="graylisted">${newWord}</span>`;
+        } else if(blackIndex !== -1) {
+          this.result += `<span class="blacklisted">${newWord}</span>`;
+        } else if(undefinedIndex !== -1) {
+          this.result += `<span class="undefined">${newWord}</span>`;
+        } else {
+          this.result += `${newWord}`;
+        }
+      });
+
+      this.result = this.result.replace(/&nbsp;/g, '');
+
+      setTimeout(() => {
+        this.setCursor();
+      }, 0);
+    },
+    async onInput(e) {
+      this.keyword = e.target.innerText.replace(/<\/?span[^>]*>/g, '');
+      this.keyword = this.keyword.replace(/&nbsp;/g, '');
+
+      if(this.timeout === null) {
+        this.timeout = setTimeout(async () => {
+          await this.check();
+        }, this.secondsBeforeCheck * 1000);
+      } else {
+        clearTimeout(this.timeout);
+        this.timeout = setTimeout(async () => {
+          await this.check();
+        }, this.secondsBeforeCheck * 1000);
+      }
+    },
+    setCursor() {
+      let el = document.getElementById("fake-textarea");
+      let range = document.createRange();
+      let sel = window.getSelection();
+
+      sel.removeAllRanges()
+      range.selectNodeContents(el);
+      range.collapse(false);
+      sel.addRange(range);
+      el.focus();
+
+      // const innerText = el.innerText.replace(/<\/?span[^>]*>/g,"");
+      // console.log(el.childNodes[0]);
+      // range.setStart(el.childNodes[0], 5);
+      // range.collapse(true);
+      
+      // sel.removeAllRanges();
+      // sel.addRange(range);
+    }
   }
 }
 </script>
@@ -24,5 +122,44 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.fake-textarea {
+  border: 1px solid black;
+  height: 200px;
+  width: 400px;
+  text-align: left;
+  padding: .5rem;
+}
+
+.graylisted {
+  position: relative;
+}
+
+.graylisted::after {
+  content: '1';
+  vertical-align: sub;
+  font-size: x-small;
+  position: absolute;
+  top: 12px;
+  right: -4px;
+}
+
+.blacklisted {
+  text-decoration: line-through;
+  color: red;
+}
+
+.undefined {
+  text-decoration: underline;
+  text-decoration-color: gray;
+}
+
+.sub {
+  vertical-align: sub;
+  font-size: x-small;
 }
 </style>
